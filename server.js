44332b5b1bbaefc2d5a43a01642542d967cb6ac2
Channa47 <103635080+Channa47@users.noreply.github.com>
2023-02-21 15:30:58 +0530

    middleware

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-require('dotenv').config();
 const port =  process.env.port || 8080; 
 server.use(middlewares);
 server.use(router);
